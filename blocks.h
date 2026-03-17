e3a3aa46246f017b02f10100770cfd7f59567dea
Rishi <rishibhargawa47@gmail.com>
2026-03-18 00:30:16 +0530

    updated files

--- blocks.h
+++ blocks.h
@@ -1,12 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",        "volume",         0,                10},
+	{"",        "volume",         60,               10},
 	{"",        "battery",        60,                0},
 	{"",        "network",        5,                 0},
 	{"",        "datetime",       60,                0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
-static char delim[] = " | ";
+static char delim[] = "    ";
 static unsigned int delimLen = 5;
